a4f9953b6abc5447119b4b593bd775d2e3c3c563
luiza-paloschi <luizapaloschi@gmail.com>
2022-10-23 10:03:17 -0300

    Virar e comparar cartas, fim do jogo adicioado

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const menorNumero = 4;
 const maiorNumero = 14;
 
 while(numValido == false){
-    numCartas = Number(prompt('Digite um número par entre 4 e 14 (inclusos)'));
+    numCartas = Number(prompt('Digite um número par de 4 a 14'));
     if (numCartas >= menorNumero && numCartas <= maiorNumero && numCartas % 2 == 0){
         numValido = true;
     } else{
@@ -38,7 +38,48 @@ while (contador < lista.length){
 
 function flipCard(clicado){
 
+      // Evitar a contabilização caso a pessoa clique várias vezes na mesma carta
+      if(clicado.className.includes('flipped')){
+        return;
+    }
 
+    jogadas++;
+    clicado.classList.add('flipped');   
+    if(carta[0]==undefined){
+        carta.push(clicado);
+        carta1 = carta[0].innerHTML;
+    } else if(carta[1]==undefined){
+        carta.push(clicado);
+        carta2 = carta[1].innerHTML;
+        compareCards(carta1, carta2);
+
+    }  
+}
+
+function compareCards(primeiraCarta, segundaCarta){
+    if(primeiraCarta==segundaCarta){
+        par++;
+        const lastChild1 = carta[0].lastElementChild;
+        lastChild1.classList.add('rainbow');
+        const lastChild2 = carta[1].lastElementChild;
+        lastChild2.classList.add('rainbow');
+        if (par === metade){
+            endGame(jogadas);
+       }
+        carta = [];
+    } else{
+        const a = [carta[1], carta[0]];
+        setTimeout(() => {a.forEach(card => {
+            card.classList.remove('flipped');});
+            carta = [];
+        }, 1000); 
+    }
+}
+
+function endGame (totalJogadas){
+    const audio = new Audio('gifs_imgs/aplausos.mp3');
+    audio.play();
+    setTimeout(alert, 500, `Fim de jogo! Você ganhou em ${totalJogadas} jogadas!`);
 }
 
 function embaralhar() { 
